3a1fcde182666fbb9a6f157f613ca6e706ee74c1
bennett <aerozotac@outlook.com>
2022-01-03 15:16:15 -0800

    Add easy-print ability with num & char options

--- crypt_helpers.h
+++ crypt_helpers.h
@@ -11,6 +11,11 @@ struct DecodedData {
     size_t size;
 };
 
+enum PrintType {
+    PRINT_TYPE_NUM,
+    PRINT_TYPE_CHAR
+};
+
 static long hex_to_decimal_byte(const char *data) {
     uint_fast8_t hex_quivs[2];
 
@@ -41,6 +46,22 @@ static struct DecodedData *hex_decode(const char *str, size_t len) {
     data->data[data->size] = '\0';
 
     return data;
+}
+
+static void print_decoded_data(const struct DecodedData *dd, enum PrintType pt) {
+    char *fmt_str = NULL;
+
+    if (pt == PRINT_TYPE_NUM) {
+        fmt_str = "%i ";
+    } else if (pt == PRINT_TYPE_CHAR) {
+        fmt_str = "%c ";
+    }
+
+    for (size_t i = 0; i < dd->size; ++i) {
+        printf(fmt_str, dd->data[i]);
+    }
+
+    putchar('\n');
 }
 
 static void *free_hex_decode(struct DecodedData *s) {
